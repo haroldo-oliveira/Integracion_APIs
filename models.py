dbc63f4a31718a28e1e33b422abfe5089f28e510
haroldo-oliveira <haroldo_oliveira@yahoo.com.br>
2022-08-21 03:24:15 +0200

    imports, class Info_Ciudad created

---
+++ models.py
@@ -0,0 +1,30 @@
+import db
+from sqlalchemy import Column, Integer, String, Boolean
+
+class Info_Ciudad(db.Base):
+    __tablename__ = "info_ciudad"
+    id = Column(Integer, primary_key=True)
+
+    ciudad = Column(String(50), nullable=False)
+
+    estado_cielo = Column(String(100))
+
+    temperatura = Column(Integer)
+
+    hecha = Column(Boolean)
+
+    def __init__(self, ciudad, estado_cielo, temperatura, hecha):
+        self.ciudad = ciudad
+        self.estado_cielo = estado_cielo
+        self.temperatura = temperatura
+        self.hecha = hecha
+
+    def __str__(self):
+        return """
+        id: {}\n
+        Info: {} \n
+        Estado Cielo: {}\n
+        Temperatura: {}\n
+        Hecha: ({})
+
+                """
